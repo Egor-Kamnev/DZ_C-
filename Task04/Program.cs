d34419296dfca7ff40db77f2367a858453f4d71c
Egor <egor18kamnev@yandex.ru>
2023-04-20 10:45:50 +0300

    Исправил Task04

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -11,7 +11,7 @@ Console.WriteLine("Введите второе число:");
 int numberB = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите третье число:");
 int numberC = Convert.ToInt32(Console.ReadLine());
-int max = Convert.ToInt32(numberA);
+int max = numberA;
 if (numberA > numberB)
 {
     max = numberA;
