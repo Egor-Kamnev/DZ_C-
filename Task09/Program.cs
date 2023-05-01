315264fc8c158e0a6d7751161fb20ec5cfc1c741
Egor <egor18kamnev@yandex.ru>
2023-05-01 16:48:56 +0300

    Доработал Task09

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -8,7 +8,18 @@
 
 int number = new Random().Next(10, 100);
 Console.WriteLine($"Случайное число из отрезка (10, 99) -> {number}");
-int firstDigit = number / 10;
-int secondDigit = number % 10;
-int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;
+// int firstDigit = number / 10;
+// int secondDigit = number % 10;
+// int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;
+// Console.WriteLine($"Наибольшая цифра числа -> {maxDigit}");
+
+int maxDigit = MaxDigit(number);
 Console.WriteLine($"Наибольшая цифра числа -> {maxDigit}");
+
+int MaxDigit (int num)
+{
+    int firstDigit = num / 10;
+    int secondDigit = num % 10;
+    int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;
+    return maxDigit;
+}
