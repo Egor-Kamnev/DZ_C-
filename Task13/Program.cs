872e1f125563a0197801cafe8509318c4375f8e5
Egor <egor18kamnev@yandex.ru>
2023-05-02 16:13:17 +0300

    Добавил Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру 
+// заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+int SearchThreeDigit (int num)
+{
+    while(num > 999)
+    {
+        num /= 10;
+    }   
+    return num % 10; 
+}
+
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = SearchThreeDigit(number);
+if (number < 100 )
+{
+    Console.WriteLine($"{number} -> Третьей цифры нет");
+}
+else
+{
+    Console.WriteLine($"{number} -> {result}");
+}
