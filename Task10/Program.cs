727e53458116c520e6a0b3ed29dc84d958c73f52
Egor <egor18kamnev@yandex.ru>
2023-05-02 14:50:26 +0300

    Добавил Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное 
+// число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int ToFindTowDigit (int num)
+{
+    int delThreeDigit = num / 10;
+    int delOneDigit = delThreeDigit % 10;
+    return delOneDigit;
+}
+
+Console.WriteLine("Введите трёхзачое число");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = ToFindTowDigit (number);
+Console.WriteLine(result);
