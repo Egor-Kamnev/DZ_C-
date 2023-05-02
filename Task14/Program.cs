78b2e0fd6df0f2fd9efc8749570922a96ca15995
Egor <egor18kamnev@yandex.ru>
2023-05-02 14:09:01 +0300

    Добавил Task14

---
+++ Task14/Program.cs
@@ -0,0 +1,16 @@
+﻿// 14. Напишите программу, которая принимает на
+// вход число и проверяет, кратно ли оно
+// одновременно 7 и 23.
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+bool MultipleTwoDig(int num)
+{
+    return num % 7 == 0 && num % 23 == 0;
+}
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+bool result = MultipleTwoDig(number);
+Console.Write(result ? $"{number} -> Да" : $"{number} -> Нет");
