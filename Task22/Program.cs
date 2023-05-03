a2c4cade03307b751526e20592b88f35596a6840
Egor <Egor18Kamnev@yandex.ru>
2023-05-03 19:20:55 +0300

    Добавил Task22

---
+++ Task22/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 22: Напишите программу, которая
+// принимает на вход число (N) и выдаёт таблицу
+// квадратов чисел от 1 до N.
+
+void TableAllSqares(int num)
+{
+    int index = 1;
+    while (index <= num)
+    {
+        Console.WriteLine($"{index}  {index * index}");
+        index++;
+    }
+}
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+TableAllSqares(number);
