26e4326a35127b8ca8ab2a3910aeca0d4508e17c
Egor <egor18kamnev@yandex.ru>
2023-05-24 22:12:11 +0300

    Добавил Task30

---
+++ Task30/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 30: Напишите программу, которая
+// выводит массив из 8 элементов, заполненный
+// нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+
+int[] CreateArrey(int size)
+{
+    int[] arrey = new int [size];
+    Random random = new Random();
+    for (int i = 0; i < arrey.Length; i++)
+    {
+        arrey[i] = new Random().Next( 0, 2);
+    }
+    return arrey;
+}
+
+void PrintArrey(int[] arrey)
+{
+    for (int i = 0; i < arrey.Length; i++)
+    {
+        Console.Write($"{arrey[i]} ");
+    }
+}
+
+Console.WriteLine("Введите длинну массива:");
+int LengthArrey = Convert.ToInt32(Console.ReadLine());
+int[] arr = CreateArrey(LengthArrey);
+PrintArrey(arr);
