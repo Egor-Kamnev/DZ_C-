6b5292a1c068ac1b1d1370863eb5ab2511b6631f
Egor <egor18kamnev@yandex.ru>
2023-05-25 22:26:40 +0300

    Добавил задачу 27

--- Task27/Program.cs
+++ Task27/Program.cs
@@ -5,4 +5,31 @@
 // 82 -> 10
 
 // 9012 -> 12
+int LenghNumbers (int num)
+{
+    int index = 0;
+    while (num > 0)
+    {
+        num /= 10;
+        index++;
+    }
+    return index;
+}
 
+int SumDigit (int num, int lengh)
+{
+    int sum = 0;
+    for (int i = 1; i <= lengh; i++)
+    {
+    sum += num % 10;
+    num /= 10;
+    }
+    return sum;
+}
+
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int lengh = LenghNumbers (number);
+int sumDigit = SumDigit(number,lengh);
+Console.WriteLine($"{number} -> {sumDigit}");
