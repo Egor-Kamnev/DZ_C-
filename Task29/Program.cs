b32528763a2b5e2c33292cccf087e401e05f77b9
Egor <egor18kamnev@yandex.ru>
2023-05-25 22:46:35 +0300

    Добавил задачу 29

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -3,3 +3,27 @@
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 
 // 6, 1, 33 -> [6, 1, 33]
+
+int[] CreateArrey(int size)
+{
+    int[] arrey = new int [size];
+    Random random = new Random();
+    for (int i = 0; i < arrey.Length; i++)
+    {
+        arrey[i] = random.Next(1, 100);
+    }
+    return arrey;
+}
+
+void PrintArrey(int[] arrey)
+{
+    for (int i = 0; i < arrey.Length; i++)
+    {
+        Console.Write($"{arrey[i]} ");
+    }
+}
+
+Console.Write("Вашему вниманию представлен массив из 8 элементов -> ");
+int LengthArrey = 8;
+int[] arr = CreateArrey(LengthArrey);
+PrintArrey(arr);
